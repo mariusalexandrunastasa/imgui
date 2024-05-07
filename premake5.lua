11a5f1490e9dfc3a3dc848b26ea9fad95af9d698
Marius Alexandru Nastasa <mariusalexandrunastasa@gmail.com>
2024-05-07 18:11:26 +0300

    update premake file for xyz project

--- premake5.lua
+++ premake5.lua
@@ -2,9 +2,8 @@ project "ImGui"
     kind "StaticLib"
     language "C++"
 
-
-    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+    targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("%{wks.location}/obj/" .. outputdir .. "/%{prj.name}")
 
     files
     {
